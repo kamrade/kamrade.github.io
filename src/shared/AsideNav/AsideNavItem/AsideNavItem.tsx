eb7ced9f8b969fefc643ee4b574227bccb74d561
Denis Mikhailov <denis.michailov@gmail.com>
2020-12-02 18:00:14 +1000

    Active item

--- src/shared/AsideNav/AsideNavItem/AsideNavItem.tsx
+++ src/shared/AsideNav/AsideNavItem/AsideNavItem.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 import { useLocation } from 'react-router-dom';
 import { NavLink } from 'react-router-dom';
 import { AsideElement } from 'shared/AsideElement/AsideElement';
+import classNames from 'classnames/bind';
+import s from './AsideNavItem.module.scss';
 
 export interface IAsideNavItemProps {
   title: string;
@@ -9,15 +11,21 @@ export interface IAsideNavItemProps {
   innerPadding?: number;
 }
 
+const sx = classNames.bind(s);
+
 export const AsideNavItem = ({ title, link, innerPadding }: IAsideNavItemProps) => {
 
+
   const location = useLocation();
-  location.pathname === link && console.log(title);
+  const asideNavItemClassName = sx({
+    AsideNavItem: true,
+    Active: location.pathname === link,
+  });
 
   return (
     <NavLink exact to={link}>
       <AsideElement interactive={true}>
-        <div style={{ paddingLeft: (innerPadding || 0) + 24 + 'px' }}>{title}</div>
+        <div className={asideNavItemClassName} style={{ paddingLeft: (innerPadding || 0) + 24 + 'px' }}>{title}</div>
       </AsideElement>
     </NavLink>
   );
