97771d901c5bcfa4649947beb29e12b5c9c3951b
Dennis Mikhailov <denis.michailov@gmail.com>
2020-12-28 10:15:12 +1000

    Index

---
+++ src/shared/index.ts
@@ -0,0 +1,7 @@
+import {Button} from './Button';
+import {Input} from './Input';
+import {FormRow} from './FormRow';
+import {Accordion} from './Accordion';
+import {Box} from './Box';
+
+export {Button, Input, FormRow, Accordion, Box};
