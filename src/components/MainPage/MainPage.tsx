57c1efe54e93f4504fa9d044705666f85cf184b8
Dennis <denis.michailov@gmail.com>
2021-07-09 22:10:27 +1000

    Merge Milq to Showcase project

--- src/components/MainPage/MainPage.tsx
+++ src/components/MainPage/MainPage.tsx
@@ -1,13 +1,47 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
+import s from './MainPage.module.scss';
+import classNames from "classnames/bind";
+const sx = classNames.bind(s);
 
 export const MainPage = () => {
 
 
   return (
     <div className={`container`}>
-      <h1>This is the Main Page</h1>
-      <NavLink exact to='/apps/typing'>Showcase</NavLink>
+      <header className={s.Header}>
+        <div className={s.Title}>Dennis Mikhailov</div>
+        <div className={s.Menu}>
+          <div className={s.MenuItem}>My works</div>
+          <div className={s.MenuItem}>Contacts</div>
+          <div className={s.MenuItem}>About</div>
+          <div className={s.MenuItem}><NavLink exact to='/apps/typing'>Showcase</NavLink></div>
+        </div>
+      </header>
+
+      <div className={s.Body}>
+        <h1>
+          Create <span className={s.Milq}>products</span> <br />
+          for the perfect experience
+        </h1>
+        <p className={s.Desc}>
+          I am happy to work with enthusiasts, designers, visual artists and developers who envolved in digital transformation of the World
+        </p>
+      </div>
+
+
+      <div className='row'>
+        <div className='col-md-8'>
+          <div className={s.Box}></div>
+        </div>
+        <div className='col-md-8'>
+          <div className={s.Box}></div>
+        </div>
+        <div className='col-md-8'>
+          <div className={s.Box}></div>
+        </div>
+      </div>
+
     </div>
   );
 }
