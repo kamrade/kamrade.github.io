cf39968b94635e58bcd9ed50a9f766ce4460e1b0
Dennis Michailov <denis.michailov@gmail.com>
2021-11-25 23:30:23 +1000

    Triads

--- src/components/Triads/TriadNode/TriadNode.tsx
+++ src/components/Triads/TriadNode/TriadNode.tsx
@@ -15,47 +15,135 @@ export interface ITriadNodeProps {
   fill?: TriadNodeFill;
 }
 
+const primaryColor    = '#fed914';
+const secondaryColor  = '#0bdb45';
+const tertiaryColor   = '#2b99ff';
+
 export const TriadNode: React.FC<ITriadNodeProps> = ({size, shape, color, fill}) => {
 
   const shapeClassName = sx({
     SvgShape: true,
-    Triangle: shape === 'triangle',
+    
     Lg: size === 'lg',
     Md: size === 'md',
     Sm: size === 'sm',
+    
     Primary: color === 'primary',
     Secondary: color === 'secondary',
     Tetriary: color === 'tertiary',
+    
+    Solid: fill === 'solid',
+    Outline: fill === 'outline',
+    Pattern: fill === 'pattern',
   })
+
+  const shapeFill = 
+    fill === 'solid' ? 'inherit' 
+    : fill === 'outline' ? 'transparent' 
+    : fill === 'pattern' ? `url(#pattern-${color})`
+    : 'transparent';
 
   return (
     <div className={s.TriadNode}>
+
+
       <div className={s.TriadNodeShape}>
+        
+        
         { shape === 'triangle' &&
             <div className={shapeClassName}>
-              <svg width="50" height="42" viewBox="0 0 50 42" xmlns="http://www.w3.org/2000/svg">
-                <path d="M25.0002 0L49.2489 42H0.751465L25.0002 0Z" fill="inherit"/>
-              </svg>
+              
+              {size === 'lg' &&
+                <svg width="49" height="42" viewBox="0 0 49 42" xmlns="http://www.w3.org/2000/svg">
+                  <defs>
+                    <pattern id="pattern-primary" x="0" y="0" width="5" height="5" patternUnits="userSpaceOnUse" patternTransform="rotate(-45)">
+                      <path d="M0,5H10" stroke-width="6" stroke={primaryColor} />
+                    </pattern>
+                    <pattern id="pattern-secondary" x="0" y="0" width="5" height="5" patternUnits="userSpaceOnUse" patternTransform="rotate(-45)">
+                      <path d="M0,5H10" stroke-width="6" stroke={secondaryColor} />
+                    </pattern>
+                    <pattern id="pattern-tertiary" x="0" y="0" width="5" height="5" patternUnits="userSpaceOnUse" patternTransform="rotate(-45)">
+                      <path d="M0,5H10" stroke-width="6" stroke={tertiaryColor} />
+                    </pattern>
+                  </defs>
+
+                  <path d="M24.2487 0L48.4974 42H0L24.2487 0Z" fill={shapeFill}/>
+
+                </svg>                
+              }
+
+              
+              { size === 'md' &&
+                <svg width="32" height="27" viewBox="0 0 32 27" xmlns="http://www.w3.org/2000/svg">
+                  <path d="M15.5885 0L31.1769 27H0L15.5885 0Z" fill={shapeFill}/>
+                </svg>
+              }
+              
+              { size === 'sm' &&
+              
+                <svg width="22" height="18" viewBox="0 0 22 18" xmlns="http://www.w3.org/2000/svg">
+                  <path d="M11.0002 0L21.3925 18H0.60791L11.0002 0Z" fill={shapeFill}/>
+                </svg>                
+              }
+
             </div>
         }
+
+
 
         { shape === 'square' &&
             <div className={shapeClassName}>
-              <svg width="40" height="40" viewBox="0 0 40 40" xmlns="http://www.w3.org/2000/svg">
-                <path d="M0 0H40V40H0V0Z" fill="inherit"/>
-              </svg>
+
+              { size === 'lg' &&
+                <svg width="41" height="40" viewBox="0 0 41 40" xmlns="http://www.w3.org/2000/svg">
+                  <path d="M0.248535 0H40.2485V40H0.248535V0Z" fill="inherit"/>
+                </svg>              
+              }
+
+              { size === 'md' &&
+                <svg width="31" height="30" viewBox="0 0 31 30" xmlns="http://www.w3.org/2000/svg">
+                  <rect x="0.588379" width="30" height="30" fill="inherit"/>
+                </svg>
+              }
+
+              { size === 'sm' &&
+                <svg width="20" height="20" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+                  <rect width="20" height="20" fill="inherit"/>
+                </svg>                
+              }
+
             </div>
         }
 
+
+
         { shape === 'circle' &&
             <div className={shapeClassName}>
-              <svg width="40" height="40" viewBox="0 0 40 40" xmlns="http://www.w3.org/2000/svg">
-                <path d="M40 20C40 31.0457 31.0457 40 20 40C8.9543 40 0 31.0457 0 20C0 8.9543 8.9543 0 20 0C31.0457 0 40 8.9543 40 20Z" fill="inherit"/>
-              </svg>
+
+              { size === 'lg' &&
+                <svg width="41" height="40" viewBox="0 0 41 40" xmlns="http://www.w3.org/2000/svg">
+                  <path d="M40.2485 20C40.2485 31.0457 31.2942 40 20.2485 40C9.20284 40 0.248535 31.0457 0.248535 20C0.248535 8.9543 9.20284 0 20.2485 0C31.2942 0 40.2485 8.9543 40.2485 20Z" fill="inherit"/>
+                </svg>
+              }
+
+              { size === 'md' && 
+                  <svg width="31" height="30" viewBox="0 0 31 30" xmlns="http://www.w3.org/2000/svg">
+                    <circle cx="15.5884" cy="15" r="15" fill="inherit"/>
+                  </svg>
+              }
+
+              { size === 'sm' &&
+                  <svg width="20" height="20" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+                    <circle cx="10" cy="10" r="10" fill="inherit"/>
+                  </svg>
+              }
+              
             </div>
         }
 
       </div>
+
+
     </div>
   )
 };
--- src/components/Triads/TriadsBoard/TriadsBoard.tsx
+++ src/components/Triads/TriadsBoard/TriadsBoard.tsx
@@ -4,17 +4,45 @@ import s from './TriadsBoard.module.scss';
 
 const TriadsBoard = () => (
   <div className={s.TriadsBoard}>
-    <TriadNode shape='triangle' size='lg' color='primary' />
-    <TriadNode shape='triangle' size='md' color='secondary'/>
-    <TriadNode shape='triangle' size='sm' color='tertiary'/>
+    <TriadNode shape='triangle' size='lg' color='primary' fill='solid' />
+    <TriadNode shape='triangle' size='lg' color='primary' fill='outline'/>
+    <TriadNode shape='triangle' size='lg' color='primary' fill='pattern'/>
 
-    <TriadNode shape='triangle' size='lg' />
-    <TriadNode shape='square' size='md' />
-    <TriadNode shape='circle' size='sm' />
+    <TriadNode shape='triangle' size='md' color='primary' fill='solid' />
+    <TriadNode shape='triangle' size='md' color='primary' fill='outline'/>
+    <TriadNode shape='triangle' size='md' color='primary' fill='pattern'/>
 
-    <TriadNode shape='triangle' size='lg' color='primary' />
-    <TriadNode shape='square' size='md' color='secondary'/>
-    <TriadNode shape='circle' size='sm' color='tertiary'/>
+    <TriadNode shape='triangle' size='sm' color='primary' fill='solid' />
+    <TriadNode shape='triangle' size='sm' color='primary' fill='outline'/>
+    <TriadNode shape='triangle' size='sm' color='primary' fill='pattern'/>
+
+
+
+    <TriadNode shape='triangle' size='lg' color='secondary' fill='solid' />
+    <TriadNode shape='triangle' size='lg' color='secondary' fill='outline'/>
+    <TriadNode shape='triangle' size='lg' color='secondary' fill='pattern'/>
+    
+    <TriadNode shape='triangle' size='md' color='secondary' fill='solid' />
+    <TriadNode shape='triangle' size='md' color='secondary' fill='outline'/>
+    <TriadNode shape='triangle' size='md' color='secondary' fill='pattern'/>
+
+    <TriadNode shape='triangle' size='sm' color='secondary' fill='solid' />
+    <TriadNode shape='triangle' size='sm' color='secondary' fill='outline'/>
+    <TriadNode shape='triangle' size='sm' color='secondary' fill='pattern'/>
+
+
+
+    <TriadNode shape='triangle' size='lg' color='tertiary' fill='solid' />
+    <TriadNode shape='triangle' size='lg' color='tertiary' fill='outline'/>
+    <TriadNode shape='triangle' size='lg' color='tertiary' fill='pattern'/>
+    
+    <TriadNode shape='triangle' size='md' color='tertiary' fill='solid' />
+    <TriadNode shape='triangle' size='md' color='tertiary' fill='outline'/>
+    <TriadNode shape='triangle' size='md' color='tertiary' fill='pattern'/>
+
+    <TriadNode shape='triangle' size='sm' color='tertiary' fill='solid' />
+    <TriadNode shape='triangle' size='sm' color='tertiary' fill='outline'/>
+    <TriadNode shape='triangle' size='sm' color='tertiary' fill='pattern'/>
   </div>
 );
 
