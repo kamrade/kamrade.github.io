ccd18658051cff4f866f515ccde9fc39f03977c6
Dennis Michailov <denis.michailov@gmail.com>
2021-12-06 00:50:22 +1000

    Function check your choose

--- src/components/Triads/RandomMode/RandomMode.tsx
+++ src/components/Triads/RandomMode/RandomMode.tsx
@@ -14,15 +14,31 @@ interface IRandomModeProps {
 
 const RandomMode: React.FC<IRandomModeProps> = ({ showMenu, setShowMenu }) => {
 
-  const [triads, setTriads] = useState<number[][]>(initiateTriads());
+  const [triads, setTriads] = useState<number[][]>( initiateTriads() );
   const [selection, setSelection] = useState<number[]>([]);
   const [triadsFound, setTriadsFound] = useState<ITriadsFound[]>([]);
   
-
   useEffect(() => {
     let founded = triadDetector(triads);
     setTriadsFound(founded);
   }, [triads]);
+
+  useEffect(() => {
+    if (selection.length >= 3) {
+      triadsFound.map((tr, i) => {
+        let keys = Object.keys(tr);
+        if (keys[0] === selection[0].toString() && keys[1] === selection[1].toString() && keys[2] === selection[2].toString()) {
+          console.log('OK');
+        }
+        return null;
+      });
+    }
+  }, [selection, triadsFound]);
+
+  const reset = () => {
+    setTriads( initiateTriads());
+    setSelection([]);
+  }
 
   return (
     <div className={s.RandomMode}>
@@ -35,16 +51,16 @@ const RandomMode: React.FC<IRandomModeProps> = ({ showMenu, setShowMenu }) => {
           <div className="col-8">
             <div className={s.TriadsPageContent}>
               <div className={s.TriadPageHead} onClick={() => setShowMenu(true)}>
-                
                 <span style={{position: 'relative', top: '6px', marginRight: '12px'}}>
                   <Icon color={triadPrimary} stroke={1.5} size={24} icon='hamburger' />
                 </span>
                 Triads
-
               </div>
+              
               <TriadsBoard selection={selection} setSelection={setSelection} triads={triads} />
+              
               <div className={s.TriadsActions}>
-                <button onClick={() => setTriads(initiateTriads())} className={s.TriadsButton}>Refresh</button>
+                <button onClick={reset} className={s.TriadsButton}>Refresh</button>
                 <button onClick={() => setSelection([])} className={s.TriadsButton}>Clear selection</button>
               </div>
 
